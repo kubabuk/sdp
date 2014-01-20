f81ae45f2f574f34744a9837de12034c5c797825
kubakaszyk <s1108328@inf.ed.ac.uk>
2014-01-20 00:24:14 +0000

    Fix original bug by setting classpath prior to starting eclipse. Now unknown output format

--- Vision/src/vision/RunVision.java
+++ Vision/src/vision/RunVision.java
@@ -27,12 +27,7 @@ public class RunVision {
                 } catch (Exception e) {
                         e.printStackTrace();
                 }
-                // Default to main pitch
-              /*PitchConstants pitchConstants = new PitchConstants(0);
-                GoalInfo goalInfo = new GoalInfo(pitchConstants);
-                WorldState worldState = new WorldState(goalInfo);*/
-
-                // Default values for the main vision window
+                
                 String videoDevice = "/dev/video0";
                 int width = 640;
                 int height = 480;
@@ -43,21 +38,6 @@ public class RunVision {
                 try {
                         VideoStream vStream = new VideoStream(videoDevice, width, height,
                                         channel, videoStandard, compressionQuality);
-
-                        /*DistortionFix distortionFix = new DistortionFix(pitchConstants);
-
-                         Create a new Vision object to serve the main vision window
-                        Vision vision = new Vision(worldState, pitchConstants);
-
-                         Create the Control GUI for threshold setting/etc
-                        VisionGUI gui = new VisionGUI(width, height, worldState,
-                                       pitchConstants, vStream, distortionFix);
-
-                        vStream.addReceiver(distortionFix);
-                        vStream.addReceiver(vision);
-                        distortionFix.addReceiver(gui);
-                        vision.addVisionDebugReceiver(gui);
-                        vision.addWorldStateReceiver(gui); */
                 } catch (Exception e) {
                         e.printStackTrace();
                 }
