d083fd4a3d2381923a731a7ad348fd48a8766a46
chzh1229 <chenzhang1229@googlemail.com>
2014-01-22 22:47:47 +0000

    try for first milestone

---
+++ Move1.java
@@ -0,0 +1,21 @@
+package com.mydomain;
+
+import lejos.navigation.Pilot;
+import lejos.nxt.Motor;
+
+public class Move1 {
+	Pilot pilot = new Pilot(2.1f,4.4f,Motor.A, Motor.C,true);
+	   pilot.setSpeed(720);// 2 RPM
+	        pilot.travel(12);
+	        pilot.rotate(-90);
+	        pilot.travel(-12,true);
+	        while(pilot.isMoving())Thread.yield();
+	        pilot.rotate(-90);
+	        pilot.rotateTo(270);
+	        pilot.steer(-50,180,true);
+	        while(pilot.isMoving())Thread.yield();
+	        pilot.steer(100);
+	        try{Thread.sleep(1000);}
+	   catch(InterruptedException e){}
+	        pilot.stop();
+}
---
+++ OneSensor.java
@@ -0,0 +1,58 @@
+package com.mydomain;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTMotor;
+import lejos.nxt.SensorPort;
+
+public class OneSensor {
+	//Codes for 1st Milestone
+	//By Roy
+	
+	public static void main(String[] args) {
+		//flag for if any button is pressed to end the program
+		Boolean flag = true;
+		
+		//2 sensors, sensor1 is on the right side and sensor2 on the left
+		LightSensor sensor1 = new LightSensor(SensorPort.S1);
+		LightSensor sensor2 = new LightSensor(SensorPort.S2);
+		
+		Move.setspeed(360);
+		while (flag) {
+			//display the sensor data on the LCD screen		
+			LCD.drawInt(sensor1.getLightValue(), 4, 0, 0);
+		    LCD.drawInt(sensor1.getNormalizedLightValue(), 4, 0, 1);
+		    //keep moving while the sensor1 is on the white edge and sensor2 in on the green area
+		    
+		   
+		    
+		    while (sensor1.getLightValue()<40)
+		    {
+		    	Move.forward();
+		    }
+		    Move.stop();
+		    //turn left while both of the sensors are on the white edge
+		    
+		    while (sensor1.getLightValue()>40)
+		    {
+		    	Move.turnLeft();
+		    //	Motor.A.rotate(60);
+		    //	Motor.B.rotate(-30);
+		    }
+//		    Move.stop();
+		    
+		    //do 
+		    //{
+		    //	Move.turnRight();
+		    //}
+		    //while (sensor1.getLightValue()<50&&sensor2.getLightValue()<50);
+		    //Move.stop();
+		    
+		    flag = ! Button.ESCAPE.isPressed();
+	    
+		}
+	}
+
+}
