55025ec214423b37d2ce85a6036581a08b625140
pmacgregor <s1126155@sms.ed.ac.uk>
2014-01-24 18:05:40 +0000

    Changes direction based on initial angle of incidence.

--- Milestone1/src/TwoSensors.java
+++ Milestone1/src/TwoSensors.java
@@ -6,52 +6,70 @@ import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
 
 public class TwoSensors {
+	//Codes for 1st Milestone
+	//By Roy
+	
 	public static void main(String[] args) {
 		//flag for if any button is pressed to end the program
 		Boolean flag = true;
 		
 		//2 sensors, sensor1 is on the right side and sensor2 on the left
-		LightSensor lightSensorLeft = new LightSensor(SensorPort.S1);
-		LightSensor lightSensorRight = new LightSensor(SensorPort.S2);
+		LightSensor sensor1 = new LightSensor(SensorPort.S1);
+		LightSensor sensor2 = new LightSensor(SensorPort.S2);
 		
 		int speedFast = 200;
 		int speedSlow = 50;
-		Move.setSpeedLeft(250);
-		Move.setSpeedRight(250);
-		while (flag) {
-		    if (lightSensorLeft.getLightValue() <= 40){
-		    	if (lightSensorRight.getLightValue() <= 40){
-		    		// both green
-		    		while(lightSensorLeft.getLightValue() <= 40){
-		    			Motor.A.rotate(180,true);
-		    			Motor.B.rotate(150,true);
-		    		}
-		    	} else {
-		    		// left green, right white
-		    		while(lightSensorRight.getLightValue() > 40){
-		    			Motor.A.rotate(-10,true);
-		    			Motor.B.rotate(10,true);
-		    		}
-		    	}
-		    } else {
-		    	if (lightSensorRight.getLightValue() > 40){
-		    		// both white
-		    		while(lightSensorRight.getLightValue() > 40){
-		    			Motor.A.rotate(-10,true);
-		    			Motor.B.rotate(10,true);
-		    		}
-		    		
-		    	} else {
-		    		// left white, right green
-		    		while(lightSensorLeft.getLightValue() > 40 & lightSensorRight.getLightValue() <=40){
-		    			Motor.A.rotate(180,true);
-		    			Motor.B.rotate(180,true);
-		    		}
-		    	}
-		    }
+		Move.setSpeedLeft(speedFast);
+		Move.setSpeedRight(speedFast);
+		while (sensor1.getLightValue()<=40 & sensor2.getLightValue()<=40){
+			Motor.A.rotate(180,true);
+			Motor.B.rotate(180,true);
+		}
+		if (sensor1.getLightValue()>40){
+			while (flag) {
+			    //keep moving while the sensor1 is on the white edge and sensor2 in on the green area
+				Move.setSpeedLeft(speedFast);
+				Move.setSpeedRight(speedFast);
+			    while (sensor1.getLightValue()<=40)
+			    {
+					Motor.A.rotate(180,true);
+					Motor.B.rotate(180,true);
+			    }
+			    //turn left while the sensor is` on the white edge
+				Move.setSpeedLeft(speedSlow);
+				Move.setSpeedRight(speedSlow);
+			    while (sensor1.getLightValue()>40)
+			    {
+			    	Motor.A.rotate(10,true);
+			    	Motor.B.rotate(-10,true);
+			    }
+			    
+			    flag = ! Button.ESCAPE.isPressed();
 		    
-		    flag = ! Button.ESCAPE.isPressed();
-	    
+			}	
+		} else {
+			while (flag) {
+			    //keep moving while the sensor1 is on the white edge and sensor2 in on the green area
+				Move.setSpeedLeft(speedFast);
+				Move.setSpeedRight(speedFast);
+			    while (sensor2.getLightValue()<=40)
+			    {
+					Motor.A.rotate(180,true);
+					Motor.B.rotate(180,true);
+			    }
+			    //turn left while the sensor is` on the white edge
+				Move.setSpeedLeft(speedSlow);
+				Move.setSpeedRight(speedSlow);
+			    while (sensor2.getLightValue()>40)
+			    {
+			    	Motor.A.rotate(-10,true);
+			    	Motor.B.rotate(10,true);
+			    }
+			    
+			    flag = ! Button.ESCAPE.isPressed();
+		    
+			}	
 		}
 	}
+
 }
