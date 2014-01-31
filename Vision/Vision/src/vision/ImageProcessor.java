49a07b871202b60335d27e0d0c07ad8a75ac289d
kubakaszyk <kubakaszyk@gmail.com>
2014-01-31 00:23:54 +0000

    finds red ball, changes it black. calculates center of ball

--- Vision/Vision/src/vision/ImageProcessor.java
+++ Vision/Vision/src/vision/ImageProcessor.java
@@ -2,11 +2,19 @@ package vision;
 
 public class ImageProcessor {
 
+    /* Currently this method finds the ball, and changes it to appear as black
+       on the gui. It also calculates the center of the ball with pixel values.
+       Needs to be refactored into two separate methods, ideally taking into 
+       account computational cost and splitting the method at latest point possible.
+       Also needs to take into consideration grid coordinate system, which is still
+       to be implemented.*/
+
 	public Image trackBall(BufferedImage img) {
-    // Currently this method just finds the pixels on the screen that have the highest RED value for RGB.
-    // Will need to find center of the ball.
-		//ArrayList<Integer[]> paths = new ArrayList<Integer[]>();
-		//ArrayList<int[]> red = new ArrayList<int[]>();
+
+		double redX = 0.0;
+		double redY = 0.0;
+		int count = 0;
+		
 		for (int w = 0; w < img.getWidth(); w++) {
 			for (int h = 0; h < img.getHeight(); h++) {
 				Color c = new Color(img.getRGB(w, h), true);
@@ -16,8 +24,13 @@ public class ImageProcessor {
 				
 				if (red > (blue + 130) && red > (green + 130)){
 					img.setRGB(w, h, 0);
+					redX += (double)w;
+					redY += (double)h;
+					count++;
 				}
 			}
+			if (count > 0) 
+				System.out.println("" + redX/((double)count) + "," + redY/((double)count));
 		}
 		return img;
 		
