0f54867d921363a966cf997a9322720ca4772ecd
Andrew Leith <s1024843@morvern.inf.ed.ac.uk>
2014-02-03 23:42:10 +0000

    Milestone 2 attacker files

---
+++ AttackerKick.java
@@ -0,0 +1,19 @@
+import lejos.nxt.Button;
+
+
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTMotor;
+
+public class AttackerKick {
+	
+	public static void main(String[] args) {
+
+	
+		Motor.C.setSpeed(800);
+		Motor.C.rotate(60);
+		
+
+}
+}
+
---
+++ robot/src/AttackerLab.java
@@ -0,0 +1,19 @@
+import lejos.nxt.Button;
+
+
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTMotor;
+
+public class AttackerLab {
+	
+	public static void main(String[] args) {
+
+	
+		Motor.C.setSpeed(320);
+		Motor.C.rotate(60);
+		
+
+}
+}
+
