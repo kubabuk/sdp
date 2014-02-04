04a2a4420faecb6588a889b48bf4d1fb804791a1
atsialos <s1010118@sms.ed.ac.uk>
2014-02-04 15:11:18 +0000

    Added predicates for equal and colinear points

--- Vision1/src/geometry/Point.java
+++ Vision1/src/geometry/Point.java
@@ -1,5 +1,5 @@
 package geometry;
-
+import java.lang.Math;
 public class Point {
 
 	private final double x;
@@ -14,4 +14,17 @@ public class Point {
 
 	public String toString()
 		{return "(" + getX() + "," + getY() + ")" ;}
+	
+	public boolean equals(Point p) {
+		return (this.x == p.getX() && this.y == p.getY());
+	}
+	public static boolean colinear(Point a, Point b, Point c){
+		Vector v1 = new Vector(a,b);
+		Vector v2 = new Vector(a,c);
+		double angle = v1.getOrientation()-v2.getOrientation();
+		return Math.sin(angle)==0;
+	}
+	public boolean isColinear(Vector v){
+		return colinear(this, v.getOrigin(), v.getDestination());
+	}
 	}
