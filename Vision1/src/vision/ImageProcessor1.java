3f70459605c29f29e512e7d3c72e270e4de6a4da
Patricia Dragan <s1135653@dodge.inf.ed.ac.uk>
2014-02-05 18:17:26 +0000

    ball speed added

--- Vision1/src/vision/ImageProcessor1.java
+++ Vision1/src/vision/ImageProcessor1.java
@@ -4,17 +4,19 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
-<<<<<<< HEAD
-public class ImageProcessor {
-=======
 
 public class ImageProcessor1 {
-	
+
 	static Color green = new Color(0,255,0);
 	static Color red = new Color(255,0,0);
 	static Color yellow = new Color(255,255,0);
 	static Color blue = new Color(0,0,255);
->>>>>>> 4c84a8810acbfdd71e525d9ecbb615aa1f4e40e7
+	
+	// using those variables to calculate the spped of the ball
+	double pavgRX = 0;
+	double pavgRY = 0;
+	double speedX = 0;
+	double speedY = 0;
 /*
      Currently this method finds the ball, and changes it to appear as black
     on the gui. It also calculates the center of the ball with pixel values.
@@ -22,23 +24,6 @@ public class ImageProcessor1 {
     account computational cost and splitting the method at latest point possible.
     Also needs to take into consideration grid coordinate system, which is still
     to be implemented. -- Kuba  
-<<<<<<< HEAD
-*/
-	// Copy this into whatever creates an imageProcesser object.
-	double pavgRX = 0;
-	double pavgRY = 0;
-	double speedX = 0;
-	double speedY = 0;
-	
-	public Image trackBall(BufferedImage img) {
-		
-		double redX = 0.0;
-		double redY = 0.0;
-		double avgRX = -1.0;
-		double avgRY = -1.0;
-		int count = 0;
-			
-=======
     
     
     more compact version - looping through the pixels less times -- Patricia
@@ -55,8 +40,7 @@ public class ImageProcessor1 {
 		int[] coords_blue = {1,1,1,1};
 
 		World newWorld = new World(img);
-		
->>>>>>> 4c84a8810acbfdd71e525d9ecbb615aa1f4e40e7
+
 		for (int w = 0; w < img.getWidth(); w++) {
 			for (int h = 0; h < img.getHeight(); h++) {
 				Color c = new Color(img.getRGB(w, h), true);
@@ -65,37 +49,12 @@ public class ImageProcessor1 {
 				int red = c.getRed();
 
 				if (red > (blue + 130) && red > (green + 130)){
-<<<<<<< HEAD
-					img.setRGB(w, h, 0);
-					redX += (double)w;
-					redY += (double)h;
-					count++;
-				}
-			}
-		}
-		if (count > 0) {
-			avgRX = redX/((double)count);
-			avgRY = redY/((double)count);
-			//System.out.println("" + avgRX + "," + avgRY);
-		}
-		
-			speedX = (pavgRX - avgRX)*20;
-			speedY = (pavgRY - avgRY)*20;
-			pavgRX = avgRX;
-			pavgRY = avgRY;
-			
-			System.out.println(speedX + " pixels per second X," + speedY + " pixels per second Y");
-		
-		return img;
-
-	}	
-=======
 //					img.setRGB(w, h, 0);
 					redX += (double)w;
 					redY += (double)h;
 					countRed++;
 				}
-				
+
 				// finding coordinates for YELLOW robots [ 0=leftx 1=lefty 2=rightx 3=righty ]
 				if (red > (blue + 55) && green > (blue + 55))
 				{
@@ -112,7 +71,7 @@ public class ImageProcessor1 {
 							coords_yellow[3] = h;
 					}
 				}
-				
+
 				// finding coordinates for BLUE robots [ 0=leftx 1=lefty 2=rightx 3=righty ]
 				if (blue > (green) && blue > (red+30) && green > (red + 25))
 				{
@@ -134,19 +93,19 @@ public class ImageProcessor1 {
 		// find red dots and get the average to find out the location of the ball
 		if (countRed > 0) 
 			newWorld.setBallXY(redX/((double)countRed), redY/((double)countRed));
-		
+
 		newWorld.setYellowXY_left(coords_yellow[0],coords_yellow[1]);
 		newWorld.setYellowXY_right(coords_yellow[2],coords_yellow[3]);
-		
+
 		newWorld.setBlueXY_left(coords_yellow[0],coords_yellow[1]);
 		newWorld.setBlueXY_right(coords_yellow[2],coords_yellow[3]);
-		
+
 		// after seeting coordinates, draw the elements on the image
-		
+
 		// draw the ball
 		int x=(int) newWorld.getBallX();
 		int y=(int) newWorld.getBallY();
-		
+
 		if (x > 50 && y > 50){
 			for (int w1 = x - 20; w1 < x + 20; w1++){
 				img.setRGB(w1, y, red.getRGB());
@@ -155,8 +114,8 @@ public class ImageProcessor1 {
 				img.setRGB(x, h1, red.getRGB());
 			}			
 		}
-		
-		
+
+
 		//draw crosses on top of the yellow robots
 		if (coords_yellow[0] > 50 && coords_yellow[1] > 50 && coords_yellow[2] > 50 && coords_yellow[3] > 50){
 			for (int w2 = coords_yellow[0] - 20; w2 < coords_yellow[0] + 20; w2++){
@@ -173,12 +132,18 @@ public class ImageProcessor1 {
 			}
 		}
 
+		// calculate the speed of the ball on axis
+		speedX = (pavgRX - redX/((double)countRed))*20;
+		speedY = (pavgRY - redY/((double)countRed))*20;
+		pavgRX = redX/((double)countRed);
+		pavgRY = redY/((double)countRed);
+		
 		// set the final image
 		newWorld.setImage(img);
 		return newWorld;
 	}
-	
-	
+
+
 	//Returns an array of integers:
 	// output[0] - y value of left boundary
 	// output[1] - x value of top boundary
@@ -208,7 +173,7 @@ public class ImageProcessor1 {
 				}
 			}
 		}
-		
+
 		// Detecting right edge.
 		for (int w = width - 1; w > 0; w--){
 			Color c = new Color(img.getRGB(w, height / 2));
@@ -224,7 +189,7 @@ public class ImageProcessor1 {
 				}
 			}
 		}
-		
+
 		// Detecting top edge.
 		for (int h = 1; h < height; h++){
 			Color c = new Color(img.getRGB(width / 2, h));
@@ -240,7 +205,7 @@ public class ImageProcessor1 {
 				}
 			}
 		}
-		
+
 		// Detecting bottom edge.
 		for (int h = height - 1; h > 0; h--){
 			Color c = new Color(img.getRGB(width / 2, h));
@@ -256,14 +221,14 @@ public class ImageProcessor1 {
 				}
 			}
 		}
-		
+
 		output[0] = leftEdge;
 		output[1] = topEdge;
 		output[2] = rightEdge;
 		output[3] = bottomEdge;
 		return output;
 	}
-	
+
 	// Draws a green outline of the pitch on the given BufferedImage
 	public Image drawBoundaries(BufferedImage img){
 		int[] boundaries = getBoundaries(img);
@@ -294,7 +259,7 @@ public class ImageProcessor1 {
 		}
 		return img;
 	}
-	
+
 	public int[] getPitchSections(BufferedImage img){
 		int[] sections = {10,10,10,10,10,10};
 		int[] boundaries = getBoundaries(img);
@@ -349,11 +314,10 @@ public class ImageProcessor1 {
 		}
 		return sections;
 	}
-	
+
 	public void pitchRatio(BufferedImage img){
 		int[] boundaries = getBoundaries(img);
 		System.out.println("Width: " + (boundaries[2] - boundaries[0]));
 		System.out.println("Height: " + (boundaries[3] - boundaries[1]));
 	}
->>>>>>> 4c84a8810acbfdd71e525d9ecbb615aa1f4e40e7
 }
