3dfaa67ba94da1a37c71f76eae9d75217ce99152
kubakaszyk <kubakaszyk@gmail.com>
2014-02-10 22:42:15 +0000

    removing copies of files

--- CommandNames.java
+++
@@ -1,9 +0,0 @@
-/**
-* Those are the possible command codes
-* @author andrewleith/EgidijusSkinkys
-*
-*/
-	public enum CommandNames {
-		MOVEFORWARD, KICK, MOVEBACKWARD, CHANGEANGLE, ABORT, EXIT, PLAY_SOUND 
-	}
-
