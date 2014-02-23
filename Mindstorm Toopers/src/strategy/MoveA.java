fd0a462ddc05e25639ffb6194a2aeb72e65ff2ad
Deliagwath <PPL3000@Gmail.com>
2014-02-23 12:07:30 +0000

    asdf

--- Mindstorm Toopers/src/strategy/MoveA.java
+++ Mindstorm Toopers/src/strategy/MoveA.java
@@ -7,17 +7,12 @@ import comms.CommandNames;
 import world.Robot;
 import world.World;
 
-//import geometry.Point;
-//import geometry.Vector;
-//import commands.*;
-//import comms.CommandNames;
-//import world.Ball;
-//import world.Robot;
-//import world.World;
-
 public class MoveA {
 	
 	public static void makeCommands(World w, Goal goal, Queue aq) {
+		//Please input size of robot
+		int robotsize = 40;
+		
 		//Extract information from Goal
 		commands.CommandNames name = goal.getMove();
 		Point point = goal.getGoal();
@@ -35,9 +30,10 @@ public class MoveA {
 		double diffy = roboty - pointy;
 		int distance = (int) Math.sqrt(diffx*diffx + diffy*diffy);
 		Vector robottopoint = new Vector(robotpos,point);
+		Vector robottoball = new Vector(robotpos,ball);
 		Vector balltogoal;
 		Vector temp;
-		Command cmd = moveto(robottopoint);
+		Command cmd = movetopoint(robottopoint);
 		Command donothing = new Command(CommandNames.DONOTHING,0,0);
 		int angle = (int) Math.abs(robotori - robottopoint.getOrientation());
 		boolean leftq = false;
@@ -76,10 +72,18 @@ public class MoveA {
 			aq.add(cmd);
 			return;
 		}
+		else if (name.equals(CommandNames.CATCH)) {
+			System.out.println("Move Command: Move to ball");
+			cmd = movetoball(robottoball,robotsize);
+			aq.add(cmd);
+			cmd = new Command(CommandNames.CATCH,0,0);
+			System.out.println("Catch Command: Catch the ball");
+			aq.add(cmd);
+		}
 		else if (name.equals(CommandNames.KICK)) {
 			if (leftq) { temp = new Vector(robotpos,lowerleft); System.out.println("Moving to Lower Left Quadrant");}
 			else { temp = new Vector(robotpos,lowerright); System.out.println("Moving to Lower Right Quadrant");}
-			cmd = moveto(temp);
+			cmd = movetopoint(temp);
 			aq.add(cmd);
 			if (leftq) { balltogoal = new Vector(robotpos,leftgoal); System.out.println("Change Angle to face left goal");} //If the attacker is on the left
 			else { balltogoal = new Vector(robotpos,rightgoal); System.out.println("Change Angle to face right goal");} //If the attacker is on the right
@@ -87,13 +91,12 @@ public class MoveA {
 			aq.add(cmd);
 			if (PLACEHOLDER/*TODO If enemy defender blocks trajectory of ball*/) {
 				temp = new Vector(robotpos,upperleft);
-				cmd = moveto(temp);
+				cmd = movetopoint(temp);
 				aq.add(cmd);
 				return;
 			}
 			else {
 				cmd = new Command(CommandNames.KICK,0,0);
-				System.out.println("Kick Command: Kick put into stack");
 				aq.add(cmd);
 				return;
 			}
@@ -130,159 +133,16 @@ public class MoveA {
 		else { return false; }
 	}
 	
-	public static Command moveto(Vector robottopoint) {
+	public static Command movetopoint(Vector robottopoint) {
 		int distance = (int) robottopoint.getMagnitude();
 		int angle = (int) robottopoint.getOrientation();
-		Command cmd = new Command(commands.CommandNames.MOVE,distance,angle);
+		Command cmd = new Command(CommandNames.MOVE,distance,angle);
 		return cmd;
 	}
-//		//Initialise Variables
-//		Robot robot = w.getAttacker();
-//		Ball ball = w.getBall();
-//		double temp = 0;
-//		Vector vtemp;
-//		boolean withcatcher = false;
-//		//Point goal = w.getGoal().getPos();
-//		double robotx = robot.getPos().getX();
-//		double roboty = robot.getPos().getY();
-//		double robotori = robot.getDir().getOrientation();
-//		double ballx = ball.getPos().getX();
-//		double bally = ball.getPos().getY();
-//		double goalx = goal.getX();
-//		double goaly = goal.getY();
-//		//Variables for Angle Calculations
-//		double opposite = 1.0;
-//		double adjacent = 1.0;
-//		int changeangle = 0;
-//		int move = 0;
-//		//Variables for Ball Interception Calculations
-//		double ballspeed = 0;
-//		double balldirection = 0;
-//		double ballentrylocation = 0; //Y Co-ordinates
-//		double ballxspeed = 0;
-//		double ballyspeed = 0;
-//		double timetohit = 0;
-//		boolean PLACEHOLDER = true;
-		//Point enemydefender = w.getEnemyDefender().getPos();
-		
-//		Command cmd = new Command(CommandNames.DONOTHING,0,0);
-//		if (goal.getAbort()) {
-//			cmd = new Command(CommandNames.ABORT,0,0);
-//			aq.add(cmd);
-//			return;
-//		}
-//		else {
-//			//Intercept -- If ball not in quadrant -- Basically Defender basic movements
-//			if (PLACEHOLDER/*If the ball is not in the attacker's quadrant*/) {
-//				ballxspeed = ball.getDir().getX();
-//				ballyspeed = ball.getDir().getY();
-//				timetohit = Math.abs(roboty - bally) * ballyspeed;
-//				move = (int) (timetohit * ballyspeed);
-//				//Go Right
-//				if (ballyspeed > 0) { cmd = new Command(CommandNames.MOVEFORWARD,move,(int) (90 - robotori)); }
-//				//Go Left
-//				else { cmd = new Command(CommandNames.MOVEBACKWARD,move,(int) (270 - robotori)); }
-//				aq.add(cmd);
-//				return;
-//			}
-//			//Catch or Manoeuvre -- If ball in quadrant && ball not moving
-//			else if (PLACEHOLDER/*Ball in Quadrant and not moving (After Interception) within a few px*/) {
-//				if (withcatcher/*Aligned towards the ball with 27.5px distance*/) {
-//					cmd = new Command(CommandNames.CATCH,0,0);
-//					aq.add(cmd);
-//					return;
-//				}
-//				else if (withcatcher/*Not Aligned towards the ball*/) {
-//					vtemp = new Vector(robot.getPos(),ball.getPos());
-//					changeangle = (int) (vtemp.getOrientation() - robotori);
-//					cmd = new Command(CommandNames.CHANGEANGLE,0,changeangle);
-//					aq.add(cmd);
-//					return;
-//				}
-//				else { //Without catcher
-//					//Move somehow behind to ball to align with goal to kick??
-//				}
-//			}
-//			//Aligned with goal, Aligned with Ricochet to Goal, Unaligned -- Ball Caught || ball not moving
-//			else if (!ball.isMoving() || Caught/*Ball not moving/Caught*/) {
-//				if (PLACEHOLDER/*Robot aligned to Ricochet into Goal*/) {
-//					cmd = new Command(CommandNames.KICK,0,0);
-//					aq.add(cmd);
-//					return;
-//				}
-//				else if (PLACEHOLDER/*Robot aligned to Goal and no enemy blocking way*/) {
-//					cmd = new Command(CommandNames.KICK,0,0);
-//					aq.add(cmd);
-//					return;
-//				}
-//				else /*Robot aligned to Goal and enemy blocking way*/ {
-//					//Align robot to ricochet into goal
-//					opposite = Math.abs(w.getAttackerFieldLeft().getPos().getY() - roboty);
-//					adjacent = (Math.abs(goalx - robotx)) / 2;
-//					changeangle = (int) Math.tan(opposite/adjacent);
-//					if (PLACEHOLDER/*On Left of Field*/) { cmd = new Command(CommandNames.CHANGEANGLE,0,-changeangle); }
-//					else /*On Right of Field*/ { cmd = new Command(CommandNames.CHANGEANGLE,0,changeangle); }
-//					aq.add(cmd);
-//					return;
-//				}
-//			}
-			
-			
-			
-			
-//			if (PLACEHOLDER/*Robot has ball*/) {
-//				if (PLACEHOLDER/*Robot aligned to ricochet into goal*/) {
-//					cmd = new Command(CommandNames.KICK,0,0);
-//					aq.add(cmd);
-//					return;
-//				}
-//				if (PLACEHOLDER/*Robot aligned towards enemy goal*/) {
-//					if (PLACEHOLDER/*Enemy Robot on same side as attacker*/) {
-//						//Align robot to ricochet into goal
-//						opposite = Math.abs(w.getAttackerFieldLeft().getPos().getY() - roboty);
-//						adjacent = (Math.abs(goalx - robotx)) / 2;
-//						changeangle = (int) Math.tan(opposite/adjacent);
-//						if (PLACEHOLDER/*On Left of Field*/) { cmd = new Command(CommandNames.CHANGEANGLE,0,-changeangle); }
-//						else /*On Right of Field*/ { cmd = new Command(CommandNames.CHANGEANGLE,0,changeangle); }
-//						aq.add(cmd);
-//						return;
-//					}
-//					else /*Enemy Robot not blocking*/ {
-//						cmd = new Command(CommandNames.KICK,0,0);
-//						aq.add(cmd);
-//						return;
-//					}
-//				}
-//				if (PLACEHOLDER/*Enemy Robot on same side as attacker)*/) {
-//					//Move towards other side of quadrant
-//					//Assuming split quadrant to left and right side
-//					//Need size position of quadrant
-//				}
-//				else /*Enemy Robot on opposite side as attacker*/ {
-//					opposite = Math.abs(robotx - goalx);
-//					adjacent = Math.abs(roboty - goaly);
-//					changeangle = (int) Math.tan(opposite/adjacent);
-//					cmd = new Command(CommandNames.CHANGEANGLE,0,changeangle);
-//				}
-//			}
-//			else /*Robot doesn't have ball*/ {
-//				if (!withcatcher) {
-//					//Manoeuvre around ball and kick
-//				}
-//				if (roboty == bally && (Math.abs(robotx - ballx) < 2.5) && withcatcher) /*2.5px (1cm) away from ball so it doesn't get hit.*/ {
-//					//TODO cmd = new Command(CommandNames.CATCH,0,0);
-//					aq.add(cmd);
-//					return;
-//				}
-//				else /*Not near the ball*/ {
-//					
-//					opposite = Math.abs(roboty - bally);
-//					adjacent = Math.abs(robotx - ballx) - 27.5; //25px from center of robot, 2.5px from ball so it doesn't get hit.
-//					changeangle = (int) Math.tan(opposite/adjacent);
-//					move = (int) Math.sqrt(Math.pow(opposite,2) + Math.pow(adjacent,2));
-//					cmd = new Command(CommandNames.MOVEFORWARD,move,changeangle);
-//					aq.add(cmd);
-//					return;
-//				}
-//			}
+	public static Command movetoball(Vector robottoball, int robotsize) {
+		int distance = (int) robottoball.getMagnitude();
+		int angle = (int) robottoball.getOrientation();
+		Command cmd = new Command(CommandNames.MOVE,(distance - robotsize),angle);
+		return cmd;
 	}
+}
