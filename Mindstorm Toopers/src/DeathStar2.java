eb545cda4bcd7e62740b1f00cc39bfecc0cb59cd
kubakaszyk <kubakaszyk@gmail.com>
2014-02-25 22:40:57 +0000

    death star 2 - runner

---
+++ Mindstorm Toopers/src/DeathStar2.java
@@ -0,0 +1,59 @@
+import geometry.Angle;
+import geometry.Point;
+import geometry.Vector;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import commands.*;
+import strategy.*;
+import comms.*;
+import world.*;
+
+public class DeathStar2 {
+
+	public static void main (String[] args) throws InterruptedException, IOException{
+		boolean color = args[0].equals("yellow");
+		boolean direction = args[1].equals("right");
+		boolean flag = true;
+		
+		// Instantiate arguments for AI - World, Queue (for attacker), Queue (for defender)
+		World universe = new World(color, direction);
+		Queue aq = new Queue();
+		Queue dq = new Queue();
+		
+		AI emperor = new AI(universe, aq, dq);
+		
+		// Allow vision time to instantiate
+		Thread.sleep(10000);
+
+		MainComm theForceDef = new MainComm(1);
+		MainComm theForceAttack = new MainComm(2);
+			
+			
+		while (flag) {
+			// Update the AI with new actions for the robots. Store them in the queue.
+			emperor.update();
+				
+			System.out.println("The ball position is " + universe.getBall().getPos().getX()+" , "+universe.getBall().getPos().getY());
+				
+			// Retrieve commands from the queue.
+			Command cmdAttack = aq.pull();
+			Command cmdDefend = dq.pull();
+
+			if(!cmdDefend.isNothing()){
+				theForceDef.sendMessage(cmdDefend.getCommand(), cmdDefend.getDistance(), cmdDefend.getAngle());
+			}
+
+			if(!cmdAttack.isNothing()){
+				theForceAttack.sendMessage(cmdAttack.getCommand(), cmdAttack.getDistance(), cmdAttack.getAngle());
+			}
+				
+		}
+
+		
+	}
+
+
+
+
+}
