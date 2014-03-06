3e9f6b9d37495c22519b7ccaf6b25bb04c084c9c
EgidijusSkinkys <s1133101@sms.ed.ac.uk>
2014-03-06 17:47:11 +0000

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,15 +1,37 @@
+package com.mydomain;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import lejos.nxt.I2CPort;
+import lejos.nxt.I2CSensor;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
 
 import com.mydomain.Commands.CommandNames;
 
 import lejos.nxt.Motor;
+import lejos.robotics.LightScanner;
 
 public class Controller {
 
-
-
 	
+	
+	public static I2CSensor MOTORMUX;
+	private final byte  kickSpeed = (byte) 255;
+	private final byte forward = (byte)1;
+	private final byte backward = (byte)2;
+	private final byte off = (byte)0;
+	private final int kickerDirection = 0x01;
+	private final int kickerSpeed = 0x02;
+	private volatile boolean isKicking = false;
+	private final int mainKickTime = 600;
+	private final int mainCatchTime = 150;
+	@SuppressWarnings("deprecation")
+
+
+
+	LightSensor light = new LightSensor(SensorPort.S1);
 	final double angleConstant=1; //constant, which is used to determine amount of degrees needed to turn the robot one angle. Depends on weight/wheels of robot.
 	final double distConstant=1; // should be equal to 0.5 cm in real world // constant, which is used to determine how much move is needed to cross one square.
 	final int kickerAngle=0;// depends on kicker, how much does kicker need to kick each time.
@@ -22,6 +44,14 @@ public class Controller {
 	public Controller()
 	{
 
+		I2CPort I2Cport = SensorPort.S4; //Assign port
+		I2Cport.i2cEnable(I2CPort.STANDARD_MODE);
+
+		MOTORMUX = new I2CSensor(I2Cport);
+
+
+
+		MOTORMUX.setAddress(0xB4);
 	  Motor.A.setAcceleration(3000);
 	  Motor.B.setAcceleration(3000);
 	  Motor.C.setAcceleration(3000);
@@ -32,7 +62,7 @@ public class Controller {
 	
 	private void kick(int speed)
 	{
-		
+	   
 	   Motor.C.setSpeed(900);
 	   Motor.C.rotate(-kickerAngle);
 	   Motor.C.rotate(kickerReturn,true);
@@ -67,28 +97,63 @@ public class Controller {
 		}
 	}
 	
-	private boolean Exit() // check if there is an exit somewhere in the command list, close the robot (maybe change it to be first program)
+	private void kick()
 	{
-	   synchronized(this) {
-	   for (Commands current: commands)
-	   {
-		   if (current.commandName ==CommandNames.EXIT)
-		   {
-			   return true;
-		   }
-	   }
-	   return false;
-	   }
+
+
+		try {
+
+
+			//Move motor forward at speed 200
+			MOTORMUX.sendData(0x02,(byte) 255);
+			MOTORMUX.sendData(0x01,(byte) 2);
+
+
+
+			Thread.sleep(mainKickTime);
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException in kick");
+		} finally {
+			MOTORMUX.sendData(kickerDirection, off);
+			MOTORMUX.sendData(kickerSpeed, off);
+
+
+		}
+
 	}
-	
-	
+
+
+	private void kickercatch()
+	{// needs different constants
+		//if(isKicking){
+
+		try {
+			MOTORMUX.sendData(0x02,(byte) 255);
+			MOTORMUX.sendData(0x01,(byte) 1);
+
+			Thread.sleep(mainCatchTime);
+			System.out.println("Inside kickercatch");
+
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException in kick");
+		} finally {
+			MOTORMUX.sendData(kickerDirection, off);
+			MOTORMUX.sendData(kickerSpeed, off);
+
+			//isKicking = false;
+		}
+		//}
+	}
 
 	
 	public void run() // main thread, which works on stuff. takes a command, analyses it and does it.
 	{
+		int lightvalue=0;
 		boolean isExit=false;
 		while(!isExit)
 		{
+			lightvalue = light.getLightValue();
+			System.out.println(lightvalue);
 			Commands curcommand = null;
 			synchronized(this) {
 			if (!commands.isEmpty())
@@ -100,14 +165,27 @@ public class Controller {
 			case ABORT:
 				abort();
 				break;
+			case CATCH:
+//				if (!caught)
+				{
+				kickercatch();
+//				caught=true;
+				System.out.println("CATCHING");
+				}
+				break;
 			case KICK:
-				kick(curcommand.firstArg);
+//				if (caught)
+				{
+				kick();
+//				caught=false;
+				System.out.println("KICKING");
+				}
 				break;
 			case MOVE:
-				moveDistance(curcommand.firstArg,curcommand.secondArg,curcommand.thirdArg);
+				moveDistance(curcommand.firstArg,curcommand.secondArg);
 				break;
 			case TURN:
-				turnAngle(curcommand.firstArg,curcommand.secondArg);
+				turnAngle(curcommand.firstArg);
 				break;
 			case EXIT:
 				isExit=true;
@@ -127,7 +205,7 @@ public class Controller {
 
 
 
-	private void moveDistance(int desiredSpeed,int distance, int angle) //Move forward the distance at desiredSpeed
+	private void moveDistance(int distance, int angle) //Move forward the distance at desiredSpeed
 	{
 	//data points:
 	//0/100% motors sideway/forward=0 degrees
@@ -195,7 +273,7 @@ public class Controller {
     }
 	}
 	
-	private void turnAngle(int desiredSpeed, int turnAngle) // turn the angle
+	private void turnAngle( int turnAngle) // turn the angle
 	{
 		    //setSpeed(desiredSpeed);
 		    setSpeed(75);
