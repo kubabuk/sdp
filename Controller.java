085ee5f72499bde17e7ee94c0e25e0fc317868fb
EgidijusSkinkys <s1133101@sms.ed.ac.uk>
2014-03-27 16:29:50 +0000

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -3,6 +3,7 @@ package com.mydomain;
 import java.util.ArrayList;
 import java.util.List;
 
+import sun.management.Sensor;
 import lejos.nxt.I2CPort;
 import lejos.nxt.I2CSensor;
 import lejos.nxt.LightSensor;
@@ -16,7 +17,7 @@ import lejos.robotics.LightScanner;
 public class Controller {
 
 	
-	
+	boolean caught=false;
 	public static I2CSensor MOTORMUX;
 	private final byte  kickSpeed = (byte) 255;
 	private final byte forward = (byte)1;
@@ -25,21 +26,21 @@ public class Controller {
 	private final int kickerDirection = 0x01;
 	private final int kickerSpeed = 0x02;
 	private volatile boolean isKicking = false;
-	private final int mainKickTime = 600;
-	private final int mainCatchTime = 150;
+	private final int mainKickTime = 125;
+	private final int mainCatchTime = 125;
 	@SuppressWarnings("deprecation")
 
 
 
-	LightSensor light = new LightSensor(SensorPort.S1);
-	final double angleConstant=1; //constant, which is used to determine amount of degrees needed to turn the robot one angle. Depends on weight/wheels of robot.
-	final double distConstant=1; // should be equal to 0.5 cm in real world // constant, which is used to determine how much move is needed to cross one square.
+	//LightSensor light = new LightSensor(SensorPort.S1);
+	final double angleConstant=3.1; //constant, which is used to determine amount of degrees needed to turn the robot one angle. Depends on weight/wheels of robot.
+	final double distConstant=11.75; // should be equal to 0.5 cm in real world // constant, which is used to determine how much move is needed to cross one square.
 	final int kickerAngle=0;// depends on kicker, how much does kicker need to kick each time.
 	final int kickerReturn=0;// depends on the kicker, whether it needs to return back.
 	int curAngle=0;
 	List<Commands> commands = new ArrayList<Commands>(); 
 	
-	private int speed = 300;// default speed, can be changed in mid run;
+	private int speed = 900;// default speed, can be changed in mid run;
 
 	public Controller()
 	{
@@ -106,7 +107,7 @@ public class Controller {
 
 			//Move motor forward at speed 200
 			MOTORMUX.sendData(0x02,(byte) 255);
-			MOTORMUX.sendData(0x01,(byte) 2);
+			MOTORMUX.sendData(0x01,(byte) 1);
 
 
 
@@ -129,7 +130,7 @@ public class Controller {
 
 		try {
 			MOTORMUX.sendData(0x02,(byte) 255);
-			MOTORMUX.sendData(0x01,(byte) 1);
+			MOTORMUX.sendData(0x01,(byte) 2);
 
 			Thread.sleep(mainCatchTime);
 			System.out.println("Inside kickercatch");
@@ -150,48 +151,56 @@ public class Controller {
 	{
 		int lightvalue=0;
 		boolean isExit=false;
-		while(!isExit)
+		while(true)
 		{
-			lightvalue = light.getLightValue();
-			System.out.println(lightvalue);
+	//		lightvalue = light.getLightValue();
+	//		if (lightvalue>41&&!caught)
+	//		{
+	//			caught=true;
+	//			kickercatch();
+	//			
+	//		}
 			Commands curcommand = null;
 			synchronized(this) {
+			
 			if (!commands.isEmpty())
 			curcommand = commands.remove(0);
 			}
 			if (curcommand!=null){
+			
 			switch (curcommand.commandName)
 			{
 			case ABORT:
 				abort();
 				break;
 			case CATCH:
-//				if (!caught)
+				if (!caught)
 				{
 				kickercatch();
-//				caught=true;
+				caught=true;
 				System.out.println("CATCHING");
 				}
 				break;
 			case KICK:
-//				if (caught)
+				if (caught)
 				{
 				kick();
-//				caught=false;
+				caught=false;
 				System.out.println("KICKING");
 				}
 				break;
 			case MOVE:
-				moveDistance(curcommand.firstArg,curcommand.secondArg);
+				moveDistance(curcommand.firstArg,curcommand.secondArg,curcommand.thirdArg);
 				break;
 			case TURN:
-				turnAngle(curcommand.firstArg);
+				turnAngle(curcommand.secondArg,curcommand.thirdArg);
 				break;
 			case EXIT:
 				isExit=true;
 				break;
 			}
-			}		
+			}	
+		
 		}	
 	}
 	
@@ -205,81 +214,136 @@ public class Controller {
 
 
 
-	private void moveDistance(int distance, int angle) //Move forward the distance at desiredSpeed
+	private void moveDistance(int distance,int angleDirection, int angle) //Move forward the distance at desiredSpeed
 	{
 	//data points:
 	//0/100% motors sideway/forward=0 degrees
-	//50/50 motors sideway/forward = 45 degrees
+	//50/50 motors sid0eway/forward = 45 degrees
 	//100%/0% motors sideway/forward = 90 degrees
 	//setSpeed(desiredSpeed);
 	//setSpeed(speed);
 
-	
+	if (angleDirection ==0)
+	{
+		angle=-angle;
+	}
 	
 	
 	
     if (angle>=0 &&angle <=90)
     {
-	float tempangle= ((float)angle)/90;//gives percentage of distribution	
-	Motor.A.setSpeed(speed*tempangle);
-	Motor.B.setSpeed(speed*(1-tempangle));
-	Motor.C.setSpeed(speed*(1-tempangle));
-	float percentangle = (float)Math.abs(0.5-tempangle);
-	Motor.A.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
-	Motor.B.rotate((int)((1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
-	Motor.C.rotate((int)((1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),false);
-    }
-    else if(angle>0)
-    {
-    	angle=angle-90;
+    	
     	float tempangle= ((float)angle)/90;//gives percentage of distribution	
-    	Motor.A.setSpeed(speed*(1-tempangle));
-    	Motor.B.setSpeed(speed*tempangle);
-    	Motor.C.setSpeed(speed*tempangle);
+
+    	Motor.A.setSpeed(speed*tempangle);
+    	Motor.B.setSpeed(speed*(1-tempangle));
+    	Motor.C.setSpeed(speed*(1-tempangle));
+
     	float percentangle = (float)Math.abs(0.5-tempangle);
+    	if (tempangle==1)
+    	{
+    		Motor.B.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
+    		Motor.C.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.A.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),false); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+        		
+    	}
+    	else 
+    	{
     	Motor.A.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
     	Motor.B.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
     	Motor.C.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),false);
+    	}
+    }
+    else if(angle>0)
+    {
+
+    	angle=angle-90;
+    	float tempangle= ((float)angle)/90;//gives percentage of distribution	
+
+    	Motor.A.setSpeed( speed*(1-tempangle));
+    	Motor.B.setSpeed(speed*tempangle);
+    	Motor.C.setSpeed(speed*tempangle);
+    	
+    	float percentangle = (float)Math.abs(0.5-tempangle);
+    	if (tempangle==1)
+    	{
+        	Motor.A.rotate((int)((1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+        	Motor.C.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.B.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),false); 
+    	}
+    	else{
+    	 // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+    	Motor.B.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
+    	Motor.C.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+    	Motor.A.rotate((int)((1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),false);
+    	}
     }
     else if(angle<-90)
     {
     	angle=Math.abs(angle)-90;
+
     	float tempangle= ((float)angle)/90;//gives percentage of distribution	
+
     	Motor.A.setSpeed(speed*(1-tempangle));
     	Motor.B.setSpeed(speed*tempangle);
     	Motor.C.setSpeed(speed*tempangle);
+
     	float percentangle = (float)Math.abs(0.5-tempangle);
-    	Motor.A.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
-    	Motor.B.rotate((int)(-(tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
-    	Motor.C.rotate((int)(-(tempangle)*(distance*(1.5-(percentangle)))*distConstant),false);
+    	if (tempangle==1)
+    	{
+        	Motor.A.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+        	Motor.C.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.B.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),false); 
+    	}
+    	else
+    	{
+    // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+    	Motor.B.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
+    	Motor.C.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+    	Motor.A.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),false); 
+    	}
  
     }
     else 
     {
     	angle=Math.abs(angle);
+    	
     	float tempangle= ((float)angle)/90;//gives percentage of distribution	
-    	Motor.A.setSpeed(speed*(1-tempangle));
-    	Motor.B.setSpeed(speed*tempangle);
-    	Motor.C.setSpeed(speed*tempangle);
+
+    	Motor.A.setSpeed(speed*tempangle);
+    	Motor.B.setSpeed(speed*(1-tempangle));
+    	Motor.C.setSpeed(speed*(1-tempangle));
+
     	float percentangle = (float)Math.abs(0.5-tempangle);
-    	System.out.println(percentangle);
-    	System.out.println(-(1-tempangle)*(distance*(1.5-(percentangle))));
-    	System.out.println((tempangle)*(distance*(1.5-(percentangle))));
-    	
-    	
-    	Motor.A.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
-    	Motor.B.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
-    	Motor.C.rotate((int)((tempangle)*(distance*(1.5-(percentangle)))*distConstant));
+    	if (tempangle==1)
+    	{
+         	Motor.B.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true); 
+        	Motor.C.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.A.rotate((int)(-(tempangle)*(distance*(1.5-(percentangle)))*distConstant),false); // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+        	
+    	}
+    	else
+    	{
+         	 // move forward, do not wait for it to finish, so move is simultaneous and does not turn a robot.
+        	Motor.B.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.C.rotate((int)(-(1-tempangle)*(distance*(1.5-(percentangle)))*distConstant),true);
+        	Motor.A.rotate((int)(-(tempangle)*(distance*(1.5-(percentangle)))*distConstant),false);
+    	}
+    
     }
 	}
 	
-	private void turnAngle( int turnAngle) // turn the angle
+	private void turnAngle(int angleDirection,int turnAngle) // turn the angle
 	{
+		if (angleDirection ==0)
+		{
+			turnAngle=-turnAngle;
+		}  
 		    //setSpeed(desiredSpeed);
-		    setSpeed(75);
-		   // turnAngle= (turnAngle*360/256)-180;//get an angle between -180 and 180
-		    Motor.B.rotate((int)(turnAngle*3),true);
-		    Motor.C.rotate((int)(-turnAngle*3),false);
+		   // setSpeed(75);
+	
+		    Motor.B.rotate((int)(-turnAngle*angleConstant),true);
+		    Motor.C.rotate((int)(turnAngle*angleConstant),false);
 	
 
 		    
